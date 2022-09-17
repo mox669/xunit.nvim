59c4fd8f8b12390a9edcbd9189840c29c04286c0
Oliver Brewka <olekatpyle@gmail.com>
2022-09-17 21:08:49 +0200

    core/export gather, added BufEnter event

--- lua/xunit/gather.lua
+++ lua/xunit/gather.lua
@@ -1,75 +1,80 @@
-local api = vim.api
-local q = require("vim.treesitter.query")
-local namespace = vim.api.nvim_create_namespace("xunit")
-local bufnr = api.nvim_get_current_buf()
+local M = {}
 
-local function debug(value)
-	print(vim.inspect(value))
-end
+function M.gather(bufnr)
+	-- local api = vim.api
+	local q = require("vim.treesitter.query")
+	-- local namespace = vim.api.nvim_create_namespace("xunit")
 
--- get the syntax_tree
-local language_tree = vim.treesitter.get_parser(bufnr, "c_sharp")
-local syntax_tree = language_tree:parse()
-local root = syntax_tree[1]:root()
+	local function debug(value)
+		print(vim.inspect(value))
+	end
 
--- ts queries
-local q_namespace = vim.treesitter.parse_query(
-	"c_sharp",
-	[[
+	-- get the syntax_tree
+	local language_tree = vim.treesitter.get_parser(bufnr, "c_sharp")
+	local syntax_tree = language_tree:parse()
+	local root = syntax_tree[1]:root()
+
+	-- ts queries
+	local q_namespace = vim.treesitter.parse_query(
+		"c_sharp",
+		[[
   (namespace_declaration
     name: (qualified_name) @namespace) 
 ]]
-)
+	)
 
-local q_classname = vim.treesitter.parse_query(
-	"c_sharp",
-	[[
+	local q_classname = vim.treesitter.parse_query(
+		"c_sharp",
+		[[
   (class_declaration
     name: (identifier) @class)
 ]]
-)
+	)
 
-local q_test_case = vim.treesitter.parse_query(
-	"c_sharp",
-	[[
+	local q_test_case = vim.treesitter.parse_query(
+		"c_sharp",
+		[[
     (method_declaration
       (attribute_list
         (attribute
           name: (identifier) @fact (#eq? @fact "Fact") (#offset! @fact)))
     name: (identifier) @test_case)
 ]]
-)
+	)
 
--- get namespace
-local ns
-for _, captures in q_namespace:iter_matches(root, bufnr) do
-	ns = q.get_node_text(captures[1], bufnr)
+	-- get namespace
+	local ns
+	for _, captures in q_namespace:iter_matches(root, bufnr) do
+		ns = q.get_node_text(captures[1], bufnr)
+	end
+
+	-- get class
+	local cls
+	for _, captures in q_classname:iter_matches(root, bufnr) do
+		cls = q.get_node_text(captures[1], bufnr)
+	end
+
+	-- get tests
+	local tests = {}
+	local i = 1
+	for _, captures, metadata in q_test_case:iter_matches(root, bufnr) do
+		local test = q.get_node_text(captures[2], bufnr)
+		-- collect all tests in file
+		table.insert(tests, i, { test, metadata[1].range[1], metadata })
+		i = i + 1
+	end
+
+	for key, test in pairs(tests) do
+		debug(key)
+		debug(test)
+		debug("name: " .. test[1])
+		debug("line: " .. test[2])
+		debug(type(test[2]))
+		debug(test[3])
+	end
+
+	local test_path = ns .. "." .. cls .. "." .. tests[1][1]
+	debug(test_path)
 end
 
--- get class
-local cls
-for _, captures in q_classname:iter_matches(root, bufnr) do
-	cls = q.get_node_text(captures[1], bufnr)
-end
-
--- get tests
-local tests = {}
-local i = 1
-for _, captures, metadata in q_test_case:iter_matches(root, bufnr) do
-	local test = q.get_node_text(captures[2], bufnr)
-	-- collect all tests in file
-	table.insert(tests, i, { test, metadata[1].range[1], metadata })
-	i = i + 1
-end
-
-for key, test in pairs(tests) do
-	debug(key)
-	debug(test)
-	debug("name: " .. test[1])
-	debug("line: " .. test[2])
-	debug(type(test[2]))
-	debug(test[3])
-end
-
-local test_path = ns .. "." .. cls .. "." .. tests[1][1]
-debug(test_path)
+return M
--- lua/xunit/init.lua
+++ lua/xunit/init.lua
@@ -1,4 +1,11 @@
-require("xunit.gather")
+local gather = require("xunit.gather")
+local bufnr = vim.api.nvim_get_current_buf()
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = vim.api.nvim_create_augroup("xunit-test", { clear = true }),
+	pattern = "*.cs",
+	callback = gather.gather(bufnr),
+})
 local M = {}
 
 return M
