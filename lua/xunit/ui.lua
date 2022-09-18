8f3d72338109587dcda068e04547a481fa3c13a8
Oliver Brewka <olekatpyle@gmail.com>
2022-09-18 17:45:33 +0200

    core/refactored ui virt methods

--- lua/xunit/ui.lua
+++ lua/xunit/ui.lua
@@ -1,19 +1,22 @@
 local M = {}
 
-function M.run_tests_virt(bufnr, ns, tests)
-	print("UI: " .. vim.inspect(tests))
+function M.set_virt_all(bufnr, ns, tests, virt_text)
 	for k, test in pairs(tests) do
-		-- delete previous extmarks with given id
-		vim.api.nvim_buf_del_extmark(bufnr, ns, k + 1)
-		-- create extmark
-		vim.api.nvim_buf_set_extmark(bufnr, ns, test.line, 0, {
-			id = k + 1,
-			--TODO (olekatpyle)  09/18/22 - 12:36: add custom HL-Group
-			virt_text = { { " Run test", "" } },
-			virt_text_pos = "eol",
-			ui_watched = true,
-		})
+		M.set_virt(bufnr, ns, test, k, virt_text)
 	end
+end
+
+function M.set_virt(bufnr, ns, test, k, virt_text)
+	-- delete previous extmarks with given id
+	vim.api.nvim_buf_del_extmark(bufnr, ns, k)
+	-- create extmark
+	vim.api.nvim_buf_set_extmark(bufnr, ns, test.line, 0, {
+		id = k,
+		--TODO (olekatpyle)  09/18/22 - 12:36: add custom HL-Group
+		virt_text = { { virt_text, "" } },
+		virt_text_pos = "eol",
+		ui_watched = true,
+	})
 end
 
 return M
